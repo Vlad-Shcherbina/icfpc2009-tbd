7f8b899afff463faf03fa18245776e812697c49b
irusskih <irusskih@cc3fe7c6-f534-0410-a7bd-bf3b583398bf>
2009-06-26 22:47:23 +0000

    stubs

--- pyvis/src/main.py
+++ pyvis/src/main.py
@@ -1,16 +1,29 @@
+import sys
+import os
+import struct
 import time
+from operator import *
+from math import *
 
 from visualizer import Visualizer
-from orbitvm import OrbitVM
+from orbitvm import OrbitVM, DummyVM
+from pyvmwrap import PyVM
 
 
-#vm = OrbitVM(OrbitVM.Hohmann, 1)
-#vm = OrbitVM(OrbitVM.Eccentric, 1)
-vm = OrbitVM(OrbitVM.ClearSkies, 1)
+if __name__ == '__main__':
 
-vis = Visualizer(vm)
-
-vm.run()
-
-vis.run()
-
+    #vm = OrbitVM(OrbitVM.Hohmann, 1)
+    #vm = OrbitVM(OrbitVM.Eccentric, 1)
+    vm = DummyVM(OrbitVM.ClearSkies, 1)
+    
+    assert len(sys.argv) == 2
+    fin = open(sys.argv[1],"rb")
+    data = fin.read()
+    #vm = PyVM(data, OrbitVM.Hohmann, 1)
+    
+    vis = Visualizer(vm)
+    
+    vm.run()
+    
+    vis.run()
+    
--- pyvis/src/orbitvm.py
+++ pyvis/src/orbitvm.py
@@ -14,6 +14,7 @@ class OrbitVM():
 	def __init__(self, type, config):
 		self.type = type
 		self.config = config
+		self.writeport(0x3E80, self.type+self.config)
 		return
 	
 	def gettype(self):
@@ -23,9 +24,30 @@ class OrbitVM():
 		return
 	
 	def readport(self, port):
+		return 0
+	
+	def writeport(self, port, val):
+		return 0
+	
+	def gettime(self):
+		return 0
+	
+	def nextStep(self):
+		self.t = self.t+1
+		
+class DummyVM(OrbitVM):
+
+	def __init__(self, type, config):
+		OrbitVM.__init__(self, type, config)
+		return
+	
+	def run(self):
+		return
+	
+	def readport(self, port):
 		return port*10e4-self.t*1000
 	
-	def writeport(self, port):
+	def writeport(self, port, val):
 		return 0
 	
 	def gettime(self):
