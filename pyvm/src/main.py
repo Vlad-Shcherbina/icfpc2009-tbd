7e0b63fa0f79b0efb7422d784fba73befe21b13f
vlad.shcherbina <vlad.shcherbina@cc3fe7c6-f534-0410-a7bd-bf3b583398bf>
2009-06-26 20:47:17 +0000

    execute, memory dump and disasm

--- pyvm/src/main.py
+++ pyvm/src/main.py
@@ -4,6 +4,35 @@ import struct
 from operator import *
 from math import *
 
+def instrToStr(instr):
+    dOp = instr>>28
+    if dOp != 0:
+        r1 = (instr>>14)&0x3FFF
+        r2 = instr&0x3FFF
+        fmt = {
+            1:"add mem[%(r1)04X],mem[%(r2)04X]",
+            2:"sub mem[%(r1)04X],mem[%(r2)04X]",
+            3:"mul mem[%(r1)04X],mem[%(r2)04X]",
+            4:"div mem[%(r1)04X],mem[%(r2)04X]",
+            5:"write outPort[%(r1)04X],mem[%(r2)04X]",
+            6:"phi mem[%(r1)04X],mem[%(r2)04X]",
+            }[dOp]
+    else:
+        sOp = instr>>24
+        r1 = instr&0x3FFF
+        cmpOp = (instr>>20)&15
+        cmpStr = ['<','<=','=','>=','>'][cmpOp]
+        fmt = {
+            0:'noop',
+            1:'cmp mem[%(r1)04X] %(cmpStr)s 0.0',
+            2:'sqrt mem[%(r1)04X]',
+            3:'copy mem[%(r1)04X]',
+            4:'input inPort[%(r1)04X]',
+            }[sOp]
+
+    return fmt%locals()
+
+
 class VM(object):
     def __init__(self,data):
         assert len(data)%12 == 0
@@ -25,17 +54,18 @@ class VM(object):
             self.code[i] = instr
             self.mem[i] = value
 
-        for i in range(self.size):
-            print self.code[i],self.mem[i]
-            
+#        for i in range(self.size):
+#            print self.code[i],self.mem[i]
+
     def execute(self):
         i = 0
         for i in range(self.size):
             instr = self.code[i]
             dOp = instr>>28
+                    
             if dOp != 0:
                 r1 = (instr>>14)&0x3FFF
-                r2 = instr&0x3FFF            
+                r2 = instr&0x3FFF
                 if dOp == 1:
                     self.mem[i] = self.mem[r1]+self.mem[r2]
                 elif dOp == 2:
@@ -63,7 +93,7 @@ class VM(object):
                 if sOp == 0:
                     pass
                 elif sOp == 1:
-                    cmpOp = (instr>>20)&7
+                    cmpOp = (instr>>20)&15
                     if [lt,le,eq,ge,gt][cmpOp](self.mem[r1],0.0):
                         self.status = 1
                     else:
@@ -77,6 +107,7 @@ class VM(object):
                 else:
                     assert False,'unknown S-op'
                     
+            print "%04X  %s % 0f"%(i,instrToStr(instr).ljust(30),self.mem[i])
 
                     
 
