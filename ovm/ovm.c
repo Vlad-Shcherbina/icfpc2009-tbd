31e2a30f1d91631c07673f8c8ff128dfde8c13e7
marflon <marflon@cc3fe7c6-f534-0410-a7bd-bf3b583398bf>
2009-06-26 21:50:32 +0000

    state preserving (for multiple steps)

--- ovm/ovm.c
+++ ovm/ovm.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdarg.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define SIZE 16384
 #define MASK 16383
@@ -12,8 +13,10 @@ unsigned int mem_code[SIZE];
 double mem_data[SIZE];
 double port[SIZE];
 unsigned short frames = 0;
+double task = 1001;
 char* image_file = 0;
-char* port_read = 0, *port_write = 0;
+char* ports_file = 0;
+char* memory_file = 0;
 int dump = 0, debug = 0;
 
 int process();
@@ -31,6 +34,32 @@ unsigned int strswitch( const char* s, ... ) {
 	return (t==0)?0:ret;
 }
 
+/* read size doubles from file */
+int file_read( char *filename, double *p, unsigned int size )
+{
+	FILE *f = fopen( filename, "rb" );
+	if( f == 0 )
+		return -1;
+	unsigned int rd = fread( p, sizeof(double), size, f );
+	fclose( f );
+	if( rd != size )
+		return -2;
+	return 0;
+}
+
+/* write size doubles to file */
+int file_write( char *filename, double *p, unsigned int size )
+{
+	FILE *f = fopen( filename, "wb" );
+	if( f == 0 )
+		return -1;
+	unsigned int wr = fwrite( p, sizeof(double), size, f );
+	fclose( f );
+	if( wr != size )
+		return -2;
+	return 0;
+}
+
 /* вытирайте ноги */
 int main( int argc, char *argv[] )
 {
@@ -46,7 +75,7 @@ int main( int argc, char *argv[] )
 	{
 		/* if this is an option */
 		if( argv[i][0] == '-' )
-			switch( strswitch((argv[i])+1,"u","pr","pw","d") )
+			switch( strswitch((argv[i])+1,"u","p","d","m","t") )
 			{
 				case 1: /* do full dump (instructions and memory) */
 					dump = 1;
@@ -54,16 +83,19 @@ int main( int argc, char *argv[] )
 				case 2: /* file where ports walues are */
 					i++;
 					if( i < argc )
-						port_read = argv[i];
+						ports_file = argv[i];
 					break;
-				case 3: /* where to write final port values */
-					i++;
-					if( i < argc )
-						port_write = argv[i];
-					break;
-				case 4: /* debug everything */
+				case 3: /* debug everything */
 					debug = 1;
 					break;
+				case 4: /* memory rw file (for state preserving) */
+					i++;
+					if( i < argc )
+						memory_file = argv[i];
+				case 5: /* task number */
+					i++;
+					if( i < argc )
+						task = atoi( argv[i] );
 				default:
 					printf( "option \"%s\" is invalid\n", argv[i] );
 			}
@@ -71,7 +103,7 @@ int main( int argc, char *argv[] )
 			image_file = argv[i];
 	}
 
-	/* read image */
+	/* read code and initial memory */
 	FILE* f = fopen( image_file, "rb" );
 	if( f == 0 )
 	{
@@ -94,7 +126,6 @@ int main( int argc, char *argv[] )
 		}
 		frame_addr++;
 	}
-
 	fclose( f );
 
 	if( rd != 0 )
@@ -107,10 +138,26 @@ int main( int argc, char *argv[] )
 	if( dump )
 		printf( "read %u frames\n", frame_addr );
 
-	port[0x3E80] = 1002;
+	/* read memory image */
+	if( memory_file )
+		file_read( memory_file, mem_data, SIZE );
+
+	/* read ports image */
+	if( ports_file )
+			file_read( ports_file, port, SIZE );
+	else
+		port[0x3E80] = 1002;
+	
+	/* well, this is it */
 	process();
-	process();
-	process();
+
+	/* write memory image */
+	if( memory_file )
+		file_write( memory_file, mem_data, SIZE );
+
+	/* write ports image */
+	if( ports_file )
+		file_write( ports_file, port, SIZE );
 
 	return 0;
 }
@@ -148,7 +195,7 @@ int process()
 		}
 		switch( opcode ) {
 			case 0x00: /* NOOP */
-				printf( "%04X\tNoop\n", pc ); 
+				if( debug || dump ) printf( "%04X\tNoop\n", pc ); 
 				continue;
 			/* D-type */
 			case 0x10: /* Add r1 r2 */
@@ -237,21 +284,21 @@ int process()
 				if( debug )
 					sprintf( dbg_str, "\t\t%f = sqrt(%f)", mem_data[pc], mem_data[r1] );
 				if( debug || dump )
-					printf( "%04X\tSqrt %04X%s\n", pc, r1, dbg_str );
+					printf( "%04X\tSqrt   %04X%s\n", pc, r1, dbg_str );
 				break;
 			case 0x03: /* Copy */
 				mem_data[pc] = mem_data[r1];
 				if( debug )
 					sprintf( dbg_str, "\t\t%f", mem_data[pc] );
 				if( debug || dump )
-					printf( "%04X\tCopy %04X%s\n", pc, r1, dbg_str );
+					printf( "%04X\tCopy   %04X%s\n", pc, r1, dbg_str );
 				break;
 			case 0x04: /* Input */
 				mem_data[pc] = port[r1];
 				if( debug )
 					sprintf( dbg_str, "\t\t%f", mem_data[pc] );
 				if( debug || dump )
-					printf( "%04X\tInput %04X%s\n", pc, r1, dbg_str );
+					printf( "%04X\tInput  %04X%s\n", pc, r1, dbg_str );
 				break;
 			default: /* WHAT */
 				printf( "opcode %02X @ %u is a failure\n", opcode, pc );
