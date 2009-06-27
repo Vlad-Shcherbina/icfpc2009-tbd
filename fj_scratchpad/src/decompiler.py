14258f25419028d8765f5820d82a0f17de162e5c
Fj.mail <Fj.mail@cc3fe7c6-f534-0410-a7bd-bf3b583398bf>
2009-06-27 00:18:36 +0000

    nice desu

--- fj_scratchpad/src/decompiler.py
+++ fj_scratchpad/src/decompiler.py
@@ -1,30 +1,13 @@
 from struct import unpack_from
-import operator
-import math
+import itertools
+from instructions import asm, Operation
 
-class asm(object):
-    add = 'add'
-    sub = 'sub'
-    mul = 'mul'
-    div = 'div'
-    out = 'out'
-    cmv = 'cmv'
-    
-    noop = 'noop'
-    cmpz = 'cmpz'
-    sqrt = 'sqrt'
-    copy = 'copy'
-    read = 'read'
-    
-    binaryopcodes = (add, sub, mul, div, out, cmv)
-    unaryopcodes  = (noop, cmpz, sqrt, copy, read)
-    cmpzops = ('<', '<=', '==', '>=', '>')
-    cmpzfuncs = (operator.__lt__,
-                 operator.__le__,
-                 operator.__eq__,
-                 operator.__ge__,
-                 operator.__gt__)
-                             
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return itertools.izip(a, b)
+
 def decode_src(buf):
     l = len(buf)
     assert l % 12 == 0, "Length is not multiple of 12"
@@ -40,81 +23,8 @@ def decode_src(buf):
         data.append(value)
     return code, data
 
-
-class BinaryOp(object):
-    def __init__(self, addr, op):
-        self.addr = addr
-        self.opcode = op >> 28
-        self.op = asm.binaryopcodes[self.opcode - 1]
-        self.r1 = op >> 14 & 0x3FFF
-        self.r2 = op & 0x3FFF
-        
-    def __str__(self):
-        return "%4d: %s  %4d %4d" % (self.addr, self.op, self.r1, self.r2)
-    
-    def execute(self, memory, input, output, status):
-        "memory is array, input/output are dictionaries, status is list of 1 element"
-        op = self.op
-        a1, a2 = memory[self.r1], memory[self.r2]
-        if   op == asm.add:
-            memory[self.addr] = a1 + a2 
-        elif op == asm.sub:
-            memory[self.addr] = a1 - a2 
-        elif op == asm.mul:
-            memory[self.addr] = a1 * a2 
-        elif op == asm.div:
-            memory[self.addr] = a1 / a2 if a2 != 0.0 else 0.0  
-        elif op == asm.out:
-            output[self.r1] = a2 
-        elif op == asm.cmv:
-            memory[self.addr] = a1 if status[0] else a2  
-        
-
-class UnaryOp(object):
-    def __init__(self, addr, op):
-        self.addr = addr
-        assert op >> 28 == 0
-        self.opcode = op >> 24
-        self.op = asm.unaryopcodes[self.opcode]
-        self.imm = op >> 21 & 0x7
-        if self.op == asm.cmpz:
-            self.cmpzop = asm.cmpzops[self.imm]
-            self.cmpzfunc = asm.cmpzfuncs[self.imm]
-        self.r1 = op & 0x3FFF
-        
-    def __str__(self):
-        c = self.op
-        if   c == asm.noop: return "noop"
-        elif c == asm.cmpz: return "cmpz %2s   %4d" % (self.cmpzop, self.r1)
-        else: return "%4d: %s %4d" % (self.addr, self.op, self.r1)
-
-    def execute(self, memory, input, output, status):
-        "memory is array, input/output are dictionaries, status is list of 1 element"
-        op = self.op
-        noop = 'noop'
-        cmpz = 'cmpz'
-        sqrt = 'sqrt'
-        copy = 'copy'
-        read = 'read'
-        if   op == asm.noop:
-            pass 
-        elif op == asm.cmpz:
-            status[0] = self.cmpzfunc(memory[self.r1], 0.0) 
-        elif op == asm.sqrt:
-            memory[self.addr] = math.sqrt(memory[self.r1]) 
-        elif op == asm.copy:
-            memory[self.addr] = memory[self.r1]  
-        elif op == asm.read:
-            memory[self.addr] = input[self.r1]
-
-
-
-def decode_op(addr, op):
-    return UnaryOp(addr, op) if op >> 28 == 0 else BinaryOp(addr, op)
-
 def decode_ops(ops):
-    return [decode_op(addr, op) for addr, op in enumerate(ops)] 
-
+    return [OperationEx(addr, op) for addr, op in enumerate(ops)] 
 
 def execute_ops(ops, data):
     memory = list(data)
@@ -130,11 +40,60 @@ def execute_ops(ops, data):
     #open('memdump.txt', 'w').writelines('%04X %f\n' % (i, f) for i, f in enumerate(memory))
     return output
 
+class OperationEx(Operation):
+    def __init__(self, *args, **kwargs):
+        Operation.__init__(self, *args, **kwargs)
+        self.uses = []
+        self.usedby = []
+    
+    def updateLink(self, op):
+        self.uses.append(op)
+        op.usedby.append(self)
+        
+    def updateLinks(self, ops):
+        op = self.op
+        if   op in (asm.add, asm.sub, asm.div, asm.mul):
+            self.updateLink(ops[self.r1])
+            self.updateLink(ops[self.r2])
+        elif op == asm.out:
+            pass
+        elif op == asm.cmv:
+            self.updateLink(ops[self.r1])
+            self.updateLink(ops[self.r2])
+            for cond in ops[self.addr - 1 :: -1]:
+                if cond.op == asm.cmpz:
+                    self.updateLink(cond)
+                    break
+            else:
+                assert False, 'cmv without cmpz (it is actually allowed, by the way)'
+        elif op in (asm.noop, asm.read):
+            pass
+        elif op in (asm.cmpz, asm.copy, asm.sqrt):
+            self.updateLink(ops[self.r1])
+        else:
+            assert False, 'WTF?!'
+    
+    def __str__(self):
+        s = Operation.__str__(self)
+        def opaddrs(lst):
+            return "[" + ", ".join(str(op.addr) for op in lst) + "]" 
+        s = s.ljust(25) + (" uses: " + opaddrs(self.uses)).ljust(30)
+        s +=" used by " + opaddrs(self.usedby)
+        return s
+        
+
+
+def analyzeops(ops):
+    for op in ops:
+        op.updateLinks(ops)
+    for op in ops:
+        print op
+
 def process_file(n):
     src = open('../../task/bin%d.obf' % n, 'rb').read()
     code, data = decode_src(src)
     ops = decode_ops(code)
-    print execute_ops(ops, data)
+    analyzeops(ops)
     
 #    outfile = open('disassembly%d' % n, 'w')    
 #    
