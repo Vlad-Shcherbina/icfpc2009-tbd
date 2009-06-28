1f3292f8ff2212183f5c1aeef9d70c9a6204f114
vlad.shcherbina <vlad.shcherbina@cc3fe7c6-f534-0410-a7bd-bf3b583398bf>
2009-06-28 11:56:21 +0000

    printing control sequence as dict

--- pyvis/src/check_sol.py
+++ pyvis/src/check_sol.py
@@ -1,6 +1,7 @@
 from __future__ import with_statement
 import sys
 import struct
+from pprint import pprint
 
 from vm import VM,teamID
 
@@ -26,6 +27,8 @@ if __name__ == '__main__':
 
     i = 12
     
+    controls = {}
+    
     while i < len(sol):
         timeStamp,numWrites = struct.unpack('<II',sol[i:i+8])
         i += 8
@@ -34,14 +37,25 @@ if __name__ == '__main__':
             vm.execute()
             step += 1
         print timeStamp,numWrites
+        controls[timeStamp] = [0.0,0.0]
         for j in range(numWrites):
             addr,value = struct.unpack("<Id",sol[i:i+12])
             i += 12
             print 'write port',addr,value
+            if timeStamp>0:
+                if addr == 1:
+                    controls[timeStamp][0] += value
+                elif addr == 2:
+                    controls[timeStamp][1] += value
             vm.writePort(addr,value) 
             # bug here if you keep same nonzero speed for two steps!
         
     assert numWrites == 0
+    
+    controls = dict((k,v) for k,v in controls.items() if v!=[0.0,0.0])
+    print 'controls =\\'
+    pprint(controls)
+    
     
     print 'SCORE IS',vm.state.score
 
