352e85ac7204f203a775e3037081bcbaac0ff993
Fj.mail <Fj.mail@cc3fe7c6-f534-0410-a7bd-bf3b583398bf>
2009-06-28 17:52:56 +0000

    'check_solution'

--- pyvis/src/check_solution.py
+++ pyvis/src/check_solution.py
@@ -7,35 +7,36 @@ from vminterface import teamID,createScenario,getSolution,parseSolution
 
 from compiled_vm import CompiledVMConstructor
 from python_vm import PythonVMConstructor 
+ctors = {
+        'compiled': CompiledVMConstructor, 
+        'python': PythonVMConstructor,
+        }
 
-
-if __name__ == '__main__':
-    ctors = {
-            'compiled': CompiledVMConstructor, 
-            'python': PythonVMConstructor,
-            }
-    assert len(sys.argv) == 4, "specify vm ({0}), .obf file and solution file".format(
-        ", ".join(ctors.keys()))
-    
-    with open(sys.argv[3],"rb") as fin:
+def check_solution(vmtype, vmfile, solutionfile):
+    with open(solutionfile, "rb") as fin:
         sol = fin.read()
     
-    scenario,controls,numSteps = parseSolution(sol)
+    scenario, controls, numSteps = parseSolution(sol)
     
     print 'scenario', scenario
     print 'numSteps', numSteps 
     print 'controls =\\'
     pprint(controls)
     
-    vm = createScenario(ctors[sys.argv[1]], sys.argv[2], scenario)
+    vm = createScenario(ctors[vmtype], vmfile, scenario)
     assert vm.state.time == 1
 
     # because we already were on step 1 after createScenario
-    steps = vm.executeSteps(numSteps-1, controls) + 1
+    steps = vm.executeSteps(numSteps - 1, controls) + 1
     
     if steps != numSteps:
         print "The machine aborted prematurely after {0} steps!".format(steps)
         
     print 'SCORE IS',vm.state.score
+    
+if __name__ == '__main__':
+    assert len(sys.argv) == 4, "specify vm ({0}), .obf file and solution file".format(
+        ", ".join(ctors.keys()))
+    check_solution(sys.argv[1], sys.argv[2], sys.argv[3])
     
     
--- pyvis/src/run_compiled_vm.py
+++ pyvis/src/run_compiled_vm.py
@@ -2,8 +2,8 @@ from compiled_vm import CompiledVMConstructor
 import time
 
 if __name__ == '__main__':
-    constructor = CompiledVMConstructor('../../task/bin4.obf')
-    vm = constructor.newvm(4001.0)
+    constructor = CompiledVMConstructor('../../task/bin2.obf')
+    vm = constructor.newvm(1001.0)
     
     clock = time.clock()   
     print vm.run(3000000, 0, 0)
